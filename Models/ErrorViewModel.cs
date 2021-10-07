399de01ffadcbf3b2507efa026cd04dc87019ab6
Loren <stinpc@hotmail.com>
2021-10-07 17:03:31 -0500

    main structure created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hotel_santa_ursula_II.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
