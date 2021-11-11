daeec42b86c5f8ce11fb91bd3f842bb77eca0695
Loren <stinpc@hotmail.com>
2021-11-11 17:53:16 -0500

    generar pdf

--- Controllers/PdfController.cs
+++ Controllers/PdfController.cs
@@ -9,56 +9,37 @@ using hotel_santa_ursula_II.Models;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 using System.IO;
-using PdfSharp;
-using Rotativa;
-using hotel_santa_ursula_II.Data;
 
 namespace hotel_santa_ursula_II.Controllers
 {
-    public class HomeController : Controller
+    public class PdfController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationDbContext _context;
 
-
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        public PdfController(ILogger<HomeController> logger)
         {
-            _context = context;
             _logger = logger;
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Nosotros()
-        {
-            return View();
-        }
-
-        public IActionResult PDf()
         {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
             PdfDocument document = new PdfDocument();
             PdfPage page = document.AddPage();
             XGraphics gfx = XGraphics.FromPdfPage(page);
             XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
-            gfx.DrawString("Hello", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            gfx.DrawString("Hello, World!", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+
+            // string filename = "HelloWorld.pdf";
+            // document.Save("C:\\");
+            // Process.Start(filename);
+            // byte[] document;
 
             MemoryStream stream = new MemoryStream(); //// 
             document.Save(stream, false); //// 
             Byte[] documentBytes = stream.ToArray(); //// 
             return File(documentBytes, "application/pdf");
-
-            // // Save the document...
-            // string filename = "HelloWorld.pdf";
-            // document.Save(filename);
-            // // ...and start a viewer.
-            // Process.Start(filename);
-            // return View(filename);
         }
-
 
 
     }
