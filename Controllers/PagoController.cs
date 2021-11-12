29f17145e16989974f8282269fc78186b5761f78
Christian Arias <chris.29.01.44@gmail.com>
2021-11-12 00:36:57 -0500

    boleta PDF

--- Controllers/PagoController.cs
+++ Controllers/PagoController.cs
@@ -10,6 +10,7 @@ using hotel_santa_ursula_II.Models;
 using Microsoft.AspNetCore.Identity;
 using OfficeOpenXml;
 using OfficeOpenXml.Table;
+using Rotativa.AspNetCore;
 
 namespace hotel_santa_ursula_II.Controllers
 {
@@ -24,10 +25,11 @@ namespace hotel_santa_ursula_II.Controllers
             _context = context;
             _userManager = userManager;
         }
-        public IActionResult Listar(){
-        var lista = _context.DataPago.ToList();
+        public IActionResult Listar()
+        {
+            var lista = _context.DataPago.ToList();
             return View(lista);
-}
+        }
         public IActionResult Create(int monto)
         {
             Pago pago = new Pago();
@@ -40,7 +42,7 @@ namespace hotel_santa_ursula_II.Controllers
         public IActionResult Pagar(Pago pago)
         {
             int a;
-            
+
             pago.FechaPago = DateTime.Now;
             _context.Add(pago);
 
@@ -49,75 +51,88 @@ namespace hotel_santa_ursula_II.Controllers
                 Include(p => p.habitacion).
                 Where(s => s.UserID.Equals(pago.UserID) && s.Status.Equals("PENDIENTE"));
 
-                
+
 
             Pedido pedido = new Pedido();
             pedido.UserID = pago.UserID;
             pedido.Total = pago.MontoTotal;
             pedido.pago = pago;
-            
+
             pedido.Estado = "PENDIENTE";
             _context.Add(pedido);
 
 
             List<Detallepedido> itemsPedido = new List<Detallepedido>();
-            foreach(var item in itemsReserva.ToList()){
+            foreach (var item in itemsReserva.ToList())
+            {
                 Detallepedido detallePedido = new Detallepedido();
-                detallePedido.pedido=pedido;
+                detallePedido.pedido = pedido;
                 detallePedido.Precio = Convert.ToInt32(item.Precio);
                 detallePedido.Habitaciones = item.habitacion;
-                a=item.habitacion.id;
+                a = item.habitacion.id;
                 detallePedido.Cantidad = item.C_noches;
                 itemsPedido.Add(detallePedido);
-                 var hab = from o in _context.habitaciones select o;
-            hab = hab.Where(n => n.id.Equals(a));
-            foreach (Models.Habitaciones j in hab.ToList())
-            {
-                
-            }
-            _context.UpdateRange(hab);
-           _context.SaveChanges();
+                var hab = from o in _context.habitaciones select o;
+                hab = hab.Where(n => n.id.Equals(a));
+                foreach (Models.Habitaciones j in hab.ToList())
+                {
+
+                }
+                _context.UpdateRange(hab);
+                _context.SaveChanges();
             }
 
             _context.AddRange(itemsPedido);
 
             foreach (Carrito p in itemsReserva.ToList())
             {
-                p.Status="PROCESADO";
+                p.Status = "PROCESADO";
             }
             _context.RemoveRange(itemsReserva);
 
             _context.SaveChanges();
             /*******/
-           /*var resultado=_context.DataProforma.Where(x=>x.Status=="PROCESADO").ToList();
-           _context.DataProforma.Remove(resultado);
-           _context.SaveChanges();*/
+            /*var resultado=_context.DataProforma.Where(x=>x.Status=="PROCESADO").ToList();
+            _context.DataProforma.Remove(resultado);
+            _context.SaveChanges();*/
             /*******/
             ViewData["Message"] = "El pago se ha registrado";
             return View("Create");
         }
 
         public IActionResult ExportarExcel()
+        {
+            string excelContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            var pagos = _context.DataPago.AsNoTracking().ToList();
+            using (var libro = new ExcelPackage())
             {
-                string excelContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-                var pagos = _context.DataPago.AsNoTracking().ToList();
-                using (var libro = new ExcelPackage())
-                    {
-                        var worksheet = libro.Workbook.Worksheets.Add("Pagos");
-                        worksheet.Cells["A1"].LoadFromCollection(pagos, PrintHeaders: true);
-                        for (var col = 1; col < pagos.Count + 1; col++)
-                            {
-                                worksheet.Column(col).AutoFit();
-                            }
-        // Agregar formato de tabla
-        var tabla = worksheet.Tables.Add(new ExcelAddressBase(fromRow: 1, fromCol: 1, toRow: pagos.Count + 1, toColumn: 2), "Pagos");
-        tabla.ShowHeader = true;
-        tabla.TableStyle = TableStyles.Light6;
-        tabla.ShowTotal = true;
+                var worksheet = libro.Workbook.Worksheets.Add("Pagos");
+                worksheet.Cells["A1"].LoadFromCollection(pagos, PrintHeaders: true);
+                for (var col = 1; col < pagos.Count + 1; col++)
+                {
+                    worksheet.Column(col).AutoFit();
+                }
+                // Agregar formato de tabla
+                var tabla = worksheet.Tables.Add(new ExcelAddressBase(fromRow: 1, fromCol: 1, toRow: pagos.Count + 1, toColumn: 2), "Pagos");
+                tabla.ShowHeader = true;
+                tabla.TableStyle = TableStyles.Light6;
+                tabla.ShowTotal = true;
 
-        return File(libro.GetAsByteArray(), excelContentType, "Pagos.xlsx");
-    }
-}
+                return File(libro.GetAsByteArray(), excelContentType, "Pagos.xlsx");
+            }
+        }
+         public async Task<IActionResult> Documento()
+        {
+           // return View(await _context.Documento.ToListAsync());
+            var userID = _userManager.GetUserName(User);
+            var items = from o in _context.DataProforma select o;
+            items = items.
+                Include(p => p.habitacion).
+                Where(s => s.UserID.Equals(userID));
+            
+           return new ViewAsPdf("Documento",await items.ToListAsync());
+        }
+
 
     }
 }
